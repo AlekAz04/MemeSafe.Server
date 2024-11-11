8b3e85f0ed92ccf14567557a70ca5b7e1abde1a7
Leonid Vakarin <l.vakarin@intellectika.ru>
2024-11-12 01:17:53 +0300

    Проверка

--- src/Authentication/MemeSafe.Authentication.AspNetCore/Extensions/ApplicationBuilderExtensions.cs
+++ src/Authentication/MemeSafe.Authentication.AspNetCore/Extensions/ApplicationBuilderExtensions.cs
@@ -2,8 +2,14 @@ using Microsoft.AspNetCore.Builder;
 
 namespace MemeSafe.AspNetCore.Extensions;
 
+/// <summary>
+/// Класс расширений для <see cref="IApplicationBuilder"/>
+/// </summary>
 public static class ApplicationBuilderExtensions
 {
+    /// <summary>
+    /// Использовать барьерную авторизацию
+    /// </summary>
     public static IApplicationBuilder UseMemeSafeAuthentication(this IApplicationBuilder builder)
     {
         builder.UseAuthentication()
